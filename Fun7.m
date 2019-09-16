510d6447d5cdbe341e260552faebab645b6b1907
oujunwei <435727748@qq.com>
2019-09-16 09:35:59 +0800

    Create Fun7.m

---
+++ Fun7.m
@@ -0,0 +1,32 @@
+%Fun7
+%Multidirectional Prediction Approach for Dynamic Multiobjective Optimization Problems
+function Fun7
+ps();
+function pf()
+t = 0;
+for t=0:0.1:1;
+    f1=0:0.01:1 ;
+    
+    Ht = 0.75*sin(0.5*pi*t) +1.25;
+    h = 1-sqrt(f1/(1+Ht))
+
+    f2= (1+Ht)*h
+    plot(f1,f2,'b','Linewidth',2); %»­¸öÌå
+    hold on;
+    pause(1);
+end;
+hold off;
+
+function ps()
+    t=0
+    for t=0:0.1:1
+        x1 = 0:0.01:1 ;
+        Gt = sin(t*pi/12)
+        xi = Gt*x1
+        plot(x1,xi,'k')
+        hold on;
+        ylim([-0.1,1]);
+        ylabel('x_i');
+        xlabel('x1');
+        pause(1);
+    end;
