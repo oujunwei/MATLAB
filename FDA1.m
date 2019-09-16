555b73a84fa45c08dbdf890d7bf33989d699c75a
oujunwei <435727748@qq.com>
2019-09-16 09:40:18 +0800

    Update FDA1.m

--- FDA1.m
+++ FDA1.m
@@ -4,8 +4,9 @@ fronty=(1-frontx.^0.5) ;
 plot(frontx,fronty,'b','Linewidth',2)
 hold on;
 hold on
+A=importdata('C:\Users\xdw-001\Documents\GitHub\cmoo\cexe\data\dynamic\PS\pf_FDA1_0_85.dat');
 
-t= 1/10;
+t= 84/10;
 gt=sin(0.5*pi*t);
 g = 1+sum((A(:,2:end)-gt).^2,2);
 h = 1-(A(:,1)./g).^0.5;
