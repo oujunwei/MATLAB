f2f19ba1d55f674c032898d8b4c02eaea5803ac3
oujunwei <435727748@qq.com>
2019-09-16 09:01:44 +0800

    Update variable.m

--- variable.m
+++ variable.m
@@ -1,10 +1,10 @@
 
 
-A=importdata('.\PS\pf_FDA1_0_49.dat');
+A=importdata('C:\Users\xdw-001\Documents\GitHub\cmoo\cexe\data\dynamic\PF\pf_FDA1_0_85.dat');
 [m,n] = size(A)
 x=1:n
 
 plot(x,A,'k'); %»­¸öÌå
-ylim([0,1]);
+xlim([1,n]);
 ylabel('Value');
 xlabel('Number of variable');
